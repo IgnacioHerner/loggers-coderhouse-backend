0a11d9ed7193c494985ee7afc7402a60e2348add
Ignacio Herner <ignah71@gmail.com>
2023-09-26 14:44:31 -0300

    probando railway

--- src/middleware/404.middleware.js
+++ src/middleware/404.middleware.js
@@ -3,7 +3,7 @@ import {Router} from 'express'
 export const router = Router()
 
 router.get('*', (req, res) => {
-    res.status(404).render("errors/404")
+    res.status(404).render("loggers-coderhouse-backend-production.up.railway.app/api/session/login")
 })
 
 export default router;

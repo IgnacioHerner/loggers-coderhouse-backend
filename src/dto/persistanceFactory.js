328c4021d39b9e98e63dbd50d78e3555d28d2636
Ignacio Herner <ignah71@gmail.com>
2023-09-26 01:50:02 -0300

    probando error railway

--- src/dto/persistanceFactory.js
+++ src/dto/persistanceFactory.js
@@ -9,32 +9,33 @@ import mongoose from 'mongoose'
 import logger from '../utils/logger.js'
 
 export const ServerUp = async (app) => {
-    try {
-        if(!MONGO_URI || !MONGO_DB_NAME || !MONGO_DB_TEST_NAME || !PORT || !PERSISTANCE){
-            throw new Error("Environment variables are not properly configured")
+    switch (PERSISTANCE) {
+      case "DEV":
+        try {
+          await mongoose.connect(MONGO_URI, {
+            dbName: MONGO_DB_NAME,
+          });
+          logger.debug("DB development connected!");
+          app.listen(PORT, () =>
+            logger.info(`Server Up on http://localhost:${PORT}/api/products\n`)
+          );
+        } catch (err) {
+          logger.fatal("Error when trying to start the server.\n", err)
         }
-
-        switch(PERSISTANCE) {
-            case "DEV":
-                await mongoose.connect(MONGO_URI, {
-                    dbName: MONGO_DB_NAME
-                })
-                logger.debug("DB Development connected")
-                break;
-            case "PROD": 
-                await mongoose.connect(MONGO_URI, {
-                    dbName: MONGO_DB_TEST_NAME
-                })
-                logger.debug("DB Production connected")
-                break
-            default: 
-                throw new Error("Invalid PERSISTANCE value.")
+        break;
+  
+      case "PROD":
+        try {
+          await mongoose.connect(ATLAS_URI, {
+            dbName: ATLAS_DB_NAME,
+          });
+          logger.debug("DB production connected!");
+          app.listen(PORT, () =>
+            logger.info(`Server Up on http://localhost:${PORT}/api/products\n`)
+          );
+        } catch (err) {
+          logger.fatal("Error when trying to start the server.\n", err)
         }
-        const port = PORT
-        app.listen(port, () => {
-            logger.info(`Server Up on http://localhost:${port}/api/products\n`)
-        })
-    }catch (err) {
-        logger.fatal("Error when trying to start the server. \n", err);
+        break;
     }
-}
+};
